2b70c9ab66077fe36a2d877a5affd6c4089c3249
Richard Kosa <rkosa@c2r1p2.42vienna.com>
2026-09-21 18:58:39 +0200

    Done ex0

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,21 @@
+def input_temperature(temp_str: str) -> int:
+        return int(temp_str)
+    
+
+def test_temperature() -> None:
+    test_values = [25, "abc"]
+    for value in test_values:
+        print(f"Input data is '{value}'")
+        try:
+            print("Temperature is now " 
+            f"{input_temperature(value)}°C")
+        except ValueError:
+            print("Caught input_temperature error: "
+            f"invalid literal for int() with base 10: '{value}'")
+        print()
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===")
+    print()
+    test_temperature()
+    print("All tests completed - program didn't crash!")
